0cf5137045bfa3a0c874de5626388e65778011bd
rjspyk5 <rjspyk5@gmail.com>
2024-08-16 11:53:34 +0600

    stuctured:products page header

--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -4,18 +4,31 @@ export const Products = () => {
       <h1>Products</h1>
 
       <div className="flex space-x-2">
-        <div className="w-1/5 border border-gray-500 shadow p-2">
-          <h1>test</h1>
+        <div className="lg:w-[20%] space-y-4 hidden lg:block *:shadow-md *:p-2 *:bg-base-100 *:rounded-sm">
+          <div className="flex  justify-between items-center">
+            <p>Filter</p>
+            <p>Clear</p>
+          </div>
+          <div></div>
         </div>
-        <div className="w-4/5 ">
-          <div className="border border-gray-500 shadow p-2 flex justify-end">
-            <select className="select select-bordered w-full max-w-xs">
-              <option disabled selected>
-                Who shot first?
-              </option>
-              <option>Han Solo</option>
-              <option>Greedo</option>
-            </select>
+        <div className="lg:w-[80%] w-full">
+          <div className=" shadow-md p-2 flex justify-between lg:justify-end bg-base-100 rounded-sm">
+            <div className="lg:hidden">filter</div>
+            <form>
+              <div className="flex gap-2 items-center">
+                <label htmlFor="select">Sort by:</label>
+                <select
+                  id="select"
+                  className="select select-bordered  h-9 focus:outline-none rounded-xl min-h-6 "
+                >
+                  <option>Default</option>
+                  <option value="">Newest</option>
+                  <option value="">Oldest</option>
+                  <option value="">Low to High</option>
+                  <option value="">High to Low</option>
+                </select>
+              </div>
+            </form>
           </div>
         </div>
       </div>
