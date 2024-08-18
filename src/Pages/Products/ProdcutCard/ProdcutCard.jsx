5f709ef3013e16ab61f61ddf3c82e7e38b4a2a31
rjspyk5 <rjspyk5@gmail.com>
2024-08-18 17:00:41 +0600

    product card details add

--- src/Pages/Products/ProdcutCard/ProdcutCard.jsx
+++ src/Pages/Products/ProdcutCard/ProdcutCard.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import { Rating } from "@smastrom/react-rating";
+import "@smastrom/react-rating/style.css";
 
 export const ProdcutCard = ({ product }) => {
   return (
@@ -7,11 +8,28 @@ export const ProdcutCard = ({ product }) => {
         <img className="h-72 w-full" src={product?.productImage} alt="Shoes" />
       </figure>
       <div className="card-body">
-        <h2 className="card-title">Shoes!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div className="card-actions justify-end">
-          <button className="btn btn-primary">Buy Now</button>
-        </div>
+        <h2 className="font-bold text-lg">{product?.productName}</h2>
+        <p className="text-lg">
+          <span className="font-semibold">Price:</span> ${product?.price}
+        </p>
+        <p className="flex">
+          <span className="font-semibold ">Rating :</span>{" "}
+          <Rating style={{ maxWidth: 80 }} value={product?.ratings} readOnly />
+        </p>
+        <p>
+          <span className="font-semibold">Description :</span>{" "}
+          {product?.description}
+        </p>
+        <p>
+          <span className="font-semibold">Brand :</span> {product?.brand}
+        </p>
+        <p>
+          <span className="font-semibold">Category :</span> {product?.category}
+        </p>
+        <p>
+          <span className="font-semibold">Creation Date :</span>{" "}
+          {product?.productCreationDate}
+        </p>
       </div>
     </div>
   );
