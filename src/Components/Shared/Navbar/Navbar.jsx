2b653ee3d64aa5d2ded339db849f1f42f9fa333c
rjspyk5 <rjspyk5@gmail.com>
2024-08-18 16:33:34 +0600

    google login functionlaity implement

--- src/Components/Shared/Navbar/Navbar.jsx
+++ src/Components/Shared/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
   const { user, logOut } = useContext(AuthContext);
-
+  console.log(user);
   return (
     <div className="navbar bg-base-300">
       <div className="flex-1">
@@ -22,25 +22,13 @@ export const Navbar = () => {
               className="btn btn-ghost btn-circle avatar"
             >
               <div className="w-10 rounded-full">
-                <img
-                  alt="Tailwind CSS Navbar component"
-                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-                />
+                <img alt="User" src={user?.photoURL} />
               </div>
             </div>
             <ul
               tabIndex={0}
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
             >
-              <li>
-                <a className="justify-between">
-                  Profile
-                  <span className="badge">New</span>
-                </a>
-              </li>
-              <li>
-                <a>Settings</a>
-              </li>
               <li>
                 <a onClick={() => logOut()}>Logout</a>
               </li>
--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -5,7 +5,7 @@ import { AuthContext } from "../../Provider/AuthProvider";
 import Swal from "sweetalert2";
 
 export const Login = () => {
-  const { user, signIn } = useContext(AuthContext);
+  const { user, signIn, googleLogin } = useContext(AuthContext);
   const navigate = useNavigate();
   const {
     register,
@@ -18,6 +18,23 @@ export const Login = () => {
 
   const onSubmit = (data) => {
     signIn(data.email, data.pass)
+      .then(() => {
+        navigate("/");
+        Swal.fire({
+          icon: "success",
+          title: "Successfully login",
+          timer: 2000,
+        });
+      })
+      .catch((er) => {
+        Swal.fire({
+          icon: "error",
+          title: `${er}`,
+        });
+      });
+  };
+  const handleGoogleLogin = () => {
+    googleLogin()
       .then(() => {
         navigate("/");
         Swal.fire({
@@ -111,7 +128,10 @@ export const Login = () => {
               </div>
             </form>{" "}
             <p className="mt-4 text-center ">or sign in with</p>
-            <button className="flex items-center w-full justify-center px-6 py-3 mt-4 text-white transition-colors duration-300 transform border rounded-lg hover:text-black hover:bg-gray-50 dark:hover:bg-gray-600">
+            <button
+              onClick={handleGoogleLogin}
+              className="flex items-center w-full justify-center px-6 py-3 mt-4 text-white transition-colors duration-300 transform border rounded-lg hover:text-black hover:bg-gray-50 dark:hover:bg-gray-600"
+            >
               <svg className="w-6 h-6 mx-2" viewBox="0 0 40 40">
                 <path
                   d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.045 27.2142 24.3525 30 20 30C14.4775 30 10 25.5225 10 20C10 14.4775 14.4775 9.99999 20 9.99999C22.5492 9.99999 24.8683 10.9617 26.6342 12.5325L31.3483 7.81833C28.3717 5.04416 24.39 3.33333 20 3.33333C10.7958 3.33333 3.33335 10.7958 3.33335 20C3.33335 29.2042 10.7958 36.6667 20 36.6667C29.2042 36.6667 36.6667 29.2042 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z"
